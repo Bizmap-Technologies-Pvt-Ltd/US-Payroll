dd2505f6dc177e58806842a741614e9ca5336cc5
priyanka-pawar-bizmap <priyanka.pawar@bizmap.in>
2025-12-27 11:12:27 +0530

    deduction components for payroll

--- us_payroll/hooks.py
+++ us_payroll/hooks.py
@@ -129,9 +129,10 @@ app_license = "mit"
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+    # "ToDo": "custom_app.overrides.CustomToDo"
+    "Salary Slip":"us_payroll.override.salary_slip.OverrideSalarySlip"
+}
 
 # Document Events
 # ---------------
---
+++ us_payroll/us_payroll/doctype/insurance_company/test_insurance_company.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, us_payroll and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestInsuranceCompany(FrappeTestCase):
+	pass
