48039cb25ad2dd4efcadd5e3d72605a9259170c5
priyanka-pawar-bizmap <priyanka.pawar@bizmap.in>
2026-05-22 12:42:12 +0530

    fix

--- us_payroll/custom_script/attendance.js
+++ us_payroll/custom_script/attendance.js
@@ -14,7 +14,7 @@ frappe.ui.form.on('Attendance', {
 	set_default_company:function(frm){
     	if (frm.doc.__islocal == 1) {
 	        frappe.call({
-	            method: 'us_payroll.us_payroll.custom_script.attendance.get_global_defaults_values',
+	            method: 'us_payroll.custom_script.attendance.get_global_defaults_values',
 	            args: {
 	                doctype: "Global Defaults",	                
 	            },
