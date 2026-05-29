a9273f6c6bad405e1febc41f0354fb8d10186053
priyanka-pawar-bizmap <priyanka.pawar@bizmap.in>
2026-05-29 11:41:23 +0530

    do_not_include_in_accounts flag

--- us_payroll/custom_script/salary_component.js
+++ us_payroll/custom_script/salary_component.js
@@ -1,5 +1,10 @@
 
 frappe.ui.form.on('Salary Component', {  
+
+    refresh: function (frm) {
+        // frm.trigger("do_not_include_in_total");
+        frm.trigger("set_flag");
+    },
     
     custom_is_this_insurance_component: function(frm) {
         if (frm.doc.custom_is_this_insurance_component) {
@@ -17,6 +22,125 @@ frappe.ui.form.on('Salary Component', {
             frm.set_value("custom_insurance_component", 0);
         }
         frm.refresh_field("custom_insurance_component");
+    },
+
+    // do_not_include_in_total: function(frm) {
+    //     frappe.call({
+    //         method: "frappe.client.get_list",
+    //         args: {
+    //             doctype: "Salary Structure",
+    //             filters: { docstatus: 1 },
+    //             fields: ["name"]
+    //         },
+    //         callback: function(res) {
+    //             if (res.message && res.message.length > 0) {
+    //                 res.message.forEach(structure => {
+                        
+    //                     // Get full Salary Structure doc
+    //                     frappe.call({
+    //                         method: "frappe.client.get",
+    //                         args: {
+    //                             doctype: "Salary Structure",
+    //                             name: structure.name
+    //                         },
+    //                         callback: function(r) {
+    //                             if (r.message && r.message.deductions) {
+                                    
+    //                                 let updated = false;
+
+    //                                 r.message.deductions.forEach(deduction => {
+    //                                     if (deduction.salary_component === frm.doc.name) {
+    //                                         deduction.do_not_include_in_total = frm.doc.do_not_include_in_total;
+    //                                         updated = true;
+    //                                     }
+    //                                 });
+
+    //                                 //Save changes if updated
+    //                                 if (updated) {
+    //                                     frappe.call({
+    //                                         method: "frappe.client.save",
+    //                                         args: {
+    //                                             doc: r.message
+    //                                         },
+    //                                         callback: function(saveRes) {
+    //                                             console.log("Updated Salary Structure:", saveRes.message.name);
+    //                                             // frm.reload_doc();
+
+    //                                         }
+    //                                     });
+    //                                 }
+    //                             }
+    //                         }
+    //                     });
+
+    //                 });
+    //             } 
+
+    //             // else {
+    //             //     frappe.msgprint("No Salary Structures found with docstatus = 1");
+    //             // }
+    //         }
+    //      });                   
+    // },
+
+    set_flag: function(frm) {
+        frappe.call({
+            method: "frappe.client.get_list",
+            args: {
+                doctype: "Salary Structure",
+                filters: { docstatus: 1 },
+                fields: ["name"]
+            },
+            callback: function(res) {
+                if (res.message && res.message.length > 0) {
+                    res.message.forEach(structure => {
+                        
+                        // Get full Salary Structure doc
+                        frappe.call({
+                            method: "frappe.client.get",
+                            args: {
+                                doctype: "Salary Structure",
+                                name: structure.name
+                            },
+                            callback: function(r) {
+                                if (r.message && r.message.deductions) {
+                                    
+                                    let updated = false;
+
+                                    r.message.deductions.forEach(deduction => {
+                                        if (deduction.salary_component === frm.doc.name) {
+                                            deduction.do_not_include_in_total = frm.doc.do_not_include_in_total;
+                                            deduction.do_not_include_in_accounts = frm.doc.do_not_include_in_accounts;
+                                            updated = true;
+                                        }
+                                    });
+
+                                    //Save changes if updated
+                                    if (updated) {
+                                        frappe.call({
+                                            method: "frappe.client.save",
+                                            args: {
+                                                doc: r.message
+                                            },
+                                            callback: function(saveRes) {
+                                                console.log("Updated Salary Structure:", saveRes.message.name);
+                                                // frm.reload_doc();
+
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        });
+
+                    });
+                } 
+
+                // else {
+                //     frappe.msgprint("No Salary Structures found with docstatus = 1");
+                // }
+            }
+         });                   
     },
 
 
