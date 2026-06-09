57f961ea3799f8a14e21c61194fc97f0a77b5850
priyanka-pawar-bizmap <priyanka.pawar@bizmap.in>
2026-06-09 17:29:58 +0530

    fix: removed workspace sidebar from hooks

--- us_payroll/hooks.py
+++ us_payroll/hooks.py
@@ -285,7 +285,7 @@ fixtures = [
 	{"dt": "Custom Field", "filters": [["module", "=", "US-Payroll"]]},
 	{"dt": "Property Setter", "filters": [["module", "=", "US-Payroll"]]},
 	{"dt": "Workspace", "filters": [["module", "=", "US-Payroll"]]},
+	{"dt": "Workspace", "filters": [["module", "=", "us_payroll"]]},
 	"Translation",
 	"Workspace",
-	"Workspace Sidebar",
 ]
