44ad493af52a3aa928dc7f1b64a140c1693d7596
priyanka-pawar-bizmap <priyanka.pawar@bizmap.in>
2026-06-10 18:36:39 +0530

    fix: removed non-used columns from report

--- us_payroll/us_payroll/report/employee_earning_reports_by_posted_date/employee_earning_reports_by_posted_date.js
+++ us_payroll/us_payroll/report/employee_earning_reports_by_posted_date/employee_earning_reports_by_posted_date.js
@@ -36,25 +36,5 @@ frappe.query_reports["Employee Earning Reports By Posted Date"] = {
 		},
 	],
 
-	onload: function (report) {
-		// report.page.add_inner_button(__("Print"), function () {
-		//        frappe.call({
-		//            method: 'us_payroll.us_payroll.report.employee_earning_reports_by_posted_date.employee_earning_reports_by_posted_date.get_print',
-		//            args: {
-		//                report_data: {'filter': report.get_values(), 'data': report.data},
-		//            },
-		//            callback: function(response) {
-		//                var response_data = response.message.pdf_file;
-		//                var bytes = new Uint8Array(response_data);
-		//                var blob = new Blob([bytes], {type: 'application/octet-stream'});
-		//                var link = document.createElement("a");
-		//                link.href = window.URL.createObjectURL(blob);
-		//                var now = new Date();
-		//                var timestamp = now.toISOString().slice(0, 19).replace(/[-T]/g, '').replace(/:/g, '');
-		//                link.download = "employee_earning_reports_by_posted_date" + timestamp + ".pdf";
-		//                link.click();
-		//            }
-		//        });
-		//    });
-	},
+	onload: function (report) {},
 };
