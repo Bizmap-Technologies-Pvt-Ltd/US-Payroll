b113cfba4a4fe29333463ea415b95fb282983c77
priyanka-pawar-bizmap <priyanka.pawar@bizmap.in>
2026-06-11 16:24:13 +0530

    fix: args str to Document

--- us_payroll/custom_script/salary_slip.py
+++ us_payroll/custom_script/salary_slip.py
@@ -1,6 +1,7 @@
 import frappe
 from erpnext.accounts.utils import get_fiscal_year
 from frappe import _
+from frappe.model.document import Document
 from frappe.utils import flt, get_url
 
 
@@ -227,7 +228,7 @@ def salary_calculations_for_fit(doc):
 
 
 @frappe.whitelist(methods=["POST"])
-def tax_calulations_for_fit(doc: str):
+def tax_calulations_for_fit(doc: Document):
 	fund_settings_doc = frappe.get_doc("Client Setup", "Client Setup")
 	total_weeks_of_the_year = fund_settings_doc.total_weeks_of_the_year
 	sal_structure = doc.salary_structure
